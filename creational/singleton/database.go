540bcdcca0a4c1be7b94c5ae052c0351841c6402
David Baena <baena5999@gmail.com>
2024-08-13 21:52:20 +0200

    Add logs

--- creational/singleton/database.go
+++ creational/singleton/database.go
@@ -5,6 +5,9 @@ import "sync"
 var instance *DatabaseConnection
 var once sync.Once
 
+// GetDatabaseConnection returns the singleton instance of DatabaseConnection.
+// It ensures that only one instance of DatabaseConnection is created and provides
+// a global point of access to it. The instance is initialized only once using sync.Once.
 func GetDatabaseConnection() *DatabaseConnection {
 	once.Do(func() {
 		instance = &DatabaseConnection{
